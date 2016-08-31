562bda9e5dda0db95d22466df59f64af032542b6
Kris Sparks <kbs5280@gmail.com>
2016-08-31 14:13:05 -0600

    Add objects to replace open_struct

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,8 @@
 class ProfilesController < ApplicationController
   def show
     @profile = Profiles.find(current_user)
-    @starred_repos = Profiles.starred_repos(current_user)
-    @recent_activity = Profiles.recent_activity(current_user)
-    @received_events = Profiles.received_events(current_user)
+    # @starred_repos = Profiles.starred_repos(current_user)
+    # @recent_activity = Profiles.recent_activity(current_user)
+    # @received_events = Profiles.received_events(current_user)
   end
 end
