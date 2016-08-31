f01a690c6cb54e51162766d79ea6f3b39720944f
Kris Sparks <kbs5280@users.noreply.github.com>
2016-08-31 09:06:41 -0600

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, "4f5fea2c22bf57d9e3a4", "8d5191b960d5559fc99132c5d0e8e72debc0eb10"
+  provider :github, "no", "nope"
 end
